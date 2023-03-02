3ebf284e2f72bd358c3f677f2be1f911c938610a
Plode <84858705+plode-v@users.noreply.github.com>
2023-03-02 15:56:11 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"), bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+
+app.get("/", (req, res) => {
+    res.render("home")
+});
+
+app.route("/login")
+    .get((req, res) => {
+        res.render("login")
+    })
+
+    .post((req, res) => {
+        const username = req.body.username
+        const password = req.body.password
+
+        User.findOne({email: username}).then(function(foundUser){
+                if (foundUser) {
+                    if (foundUser.password === password) {
+                        res.render("secrets")
+                    } else {
+                        res.send("Error")
+                    }
+                } else {
+                    res.send("err")
+                }
+            }
+        )
+
+    })
+
+
+
+
+app.route("/register")
+    .get((req, res) => {
+        res.render('register')
+    })
+
+    .post((req, res) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+
+        newUser.save().then((err) => {
+            if (!err) {
+                console.log(err)
+            } else {
+                res.render("secrets")
+            }
+        })
+    })
+
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000")
+})
